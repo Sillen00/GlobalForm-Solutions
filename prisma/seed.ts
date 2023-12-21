fb855a0929caf444e6d8ea5fadaa18a2cef30b9c
stenbumling <marcusrosin@outlook.com>
2023-12-21 16:23:59 +0100

    run prettier

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,26 +1,23 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client"
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 async function main() {
-  const testData = [
-    { name: 'Test Name 1' },
-    { name: 'Test Name 2' },
-  ];
+  const testData = [{ name: "Test Name 1" }, { name: "Test Name 2" }]
 
   for (const data of testData) {
     const testModel = await prisma.testModel.create({
       data,
-    });
-    console.log(`Created test model with id: ${testModel.id}`);
+    })
+    console.log(`Created test model with id: ${testModel.id}`)
   }
 }
 
 main()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
+  .catch(e => {
+    console.error(e)
+    process.exit(1)
   })
   .finally(async () => {
-    await prisma.$disconnect();
-  });
+    await prisma.$disconnect()
+  })
