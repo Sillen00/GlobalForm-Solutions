9a3adfb597b04fba5890e7e3641d77105f581050
stenbumling <marcusrosin@outlook.com>
2023-12-21 15:38:22 +0100

    create seed to test database

---
+++ prisma/seed.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const testData = [
+    { name: 'Test Name 1' },
+    { name: 'Test Name 2' },
+  ];
+
+  for (const data of testData) {
+    const testModel = await prisma.testModel.create({
+      data,
+    });
+    console.log(`Created test model with id: ${testModel.id}`);
+  }
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
