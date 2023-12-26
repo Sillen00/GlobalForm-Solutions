4cdfc8597d95db005f8487df55795b6f22a97628
Simon <sillen.00@hotmail.com>
2023-12-26 12:47:06 +0100

    basic styling to open / close menu on mobile.

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,16 +1,28 @@
+"use client"
 import Image from "next/image"
 import Link from "next/link"
+import { useState } from "react"
+import { IoClose, IoMenu } from "react-icons/io5"
 import "./Header.scss"
-import { IoMenu } from "react-icons/io5"
 
 function Header() {
-//   const [isLoggedin, setIsLoggedin] = useState(false) //Används inte än så länge... Förberädde för login länkarna.
+  //   const [isLoggedin, setIsLoggedin] = useState(false) //Används inte än så länge... Förberädde för login länkarna.
+  const [isMenuOpen, setMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen)
+  }
 
   return (
     <header>
       <nav>
-        <Image src='/favicon.png' alt='Global Form Solutions logo' width={50} height={50} />
-        <ul>
+        <Image
+          src='/favicon.png'
+          alt='Global Form Solutions logo'
+          width={50}
+          height={50}
+        />
+        <ul className={isMenuOpen ? "display-block" : ""}>
           <Link href='/'>Home</Link>
           <Link href='/login'>Login</Link>
           <Link href='/'>Create Account</Link>
@@ -23,9 +35,9 @@ function Header() {
           )}
         </ul>
 
-        <Link href='#' className='hamburger'>
-            <IoMenu />
-        </Link>
+        <div className='hamburger' onClick={toggleMenu}>
+          {isMenuOpen ? <IoClose /> : <IoMenu />}
+        </div>
       </nav>
     </header>
   )
