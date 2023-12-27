bcd73f898fde4e106240fa2037c70d7aa5672825
Thomas ingvarsson <105804552+GunnarIng@users.noreply.github.com>
2023-12-27 20:36:15 +0100

    add scss file

--- src/app/login/signup/page.tsx
+++ src/app/login/signup/page.tsx
@@ -1,12 +1,15 @@
 import { SignUp } from "@clerk/nextjs"
+import styles from '../signup/page.module.scss';
 
 function signUpPage() {
   return (
-    <div>
+    <div className={styles.signupContainer}>
       <h1>Sign Up Page</h1>
       <SignUp />
     </div>
-  )
+  );
 }
 
-export default signUpPage
+// apply styles to the page
+
+export default signUpPage;
