45f68a13fbde2c589ae3f41e8ce32f3a1103e121
Simon <sillen.00@hotmail.com>
2023-12-28 22:34:33 +0100

    Build up some wrapers and basic html and css code for dashboard page.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,32 @@
-import { SignedIn, UserButton } from "@clerk/nextjs"
+import { SignedIn } from "@clerk/nextjs"
+import Link from "next/link"
+import { FaPlus } from "react-icons/fa6"
+import FormCard from "../_components/FormCard"
+import "./page.scss"
 
 function DashboardPage() {
   return (
     <SignedIn>
-      <div>
-        <h1>Dashboard</h1>
-        <UserButton />
+      <div className='dashboard-wrapper'>
+        <h2>Dashboard</h2>
+        <div className='form-card-container'>
+          {/* Create new form card: */}
+          <Link href='/create-form'>
+            <div className='create-new-card'>
+              <FaPlus />
+            </div>
+          </Link>
+
+          {/* Form cards: */}
+          <FormCard date='Datum' time='Tid' title='Titel' place='Plats' />
+          <FormCard date='Datum' time='Tid' title='Titel' place='Plats' />
+          <FormCard date='Datum' time='Tid' title='Titel' place='Plats' />
+          <FormCard date='Datum' time='Tid' title='Titel' place='Plats' />
+          <FormCard date='Datum' time='Tid' title='Titel' place='Plats' />
+          <FormCard date='Datum' time='Tid' title='Titel' place='Plats' />
+          <FormCard date='Datum' time='Tid' title='Titel' place='Plats' />
+          <FormCard date='Datum' time='Tid' title='Titel' place='Plats' />
+        </div>
       </div>
     </SignedIn>
   )
