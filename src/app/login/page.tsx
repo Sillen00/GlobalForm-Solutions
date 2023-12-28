c2bf7e1c98c024792155693c2f92d153501fde40
Simon <sillen.00@hotmail.com>
2023-12-28 15:15:48 +0100

    npm run prettier and some spacing.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,13 @@
-import { SignIn } from "@clerk/nextjs";
-import React from "react";
-import styles from './page.module.scss';
+import { SignIn } from "@clerk/nextjs"
+import React from "react"
+import styles from "./page.module.scss"
 
-function  LoginPage() {
+function LoginPage() {
   return (
     <div className={styles.signInContainer}>
       <SignIn />
     </div>
-  );
+  )
 }
 
-export default LoginPage;
+export default LoginPage
