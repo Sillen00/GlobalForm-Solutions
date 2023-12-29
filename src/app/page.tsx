d5d0da0b196b9773405dcf17b2bf1d9121a96e24
Simon <sillen.00@hotmail.com>
2023-12-29 23:03:35 +0100

    Added the footer and body padding to the landing page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,69 +3,75 @@ import { IoIosArrowDroprightCircle } from "react-icons/io"
 // import { CreatePost } from "~/app/_components/create-post"
 // import { api } from "~/trpc/server"
 import Image from "next/image"
+import Footer from "./_components/Footer"
 import "./page.scss"
 
 export default async function Home() {
   // const hello = await api.post.hello.query({ text: "from tRPC" })
 
   return (
-    <main>
-      <div className='hero'>
-        <h1>
-          GlobalForm <br />
-          <span>Solutions</span>
-        </h1>
-        <div className='fake-input'></div>
+    <>
+      <div className='body-padding'>
+        <main>
+          <div className='hero'>
+            <h1>
+              GlobalForm <br />
+              <span>Solutions</span>
+            </h1>
+            <div className='fake-input'></div>
 
-        <p>
-          Create your own customized form today and share it with friends or use
-          the form as a invitation for a event or other. Create your own
-          customized form today and share it with friends or use the form as a
-          invitation for a event or other happenings!
-        </p>
-        <IoIosArrowDroprightCircle className='arrow-btn' />
+            <p>
+              Create your own customized form today and share it with friends or
+              use the form as a invitation for a event or other. Create your own
+              customized form today and share it with friends or use the form as
+              a invitation for a event or other happenings!
+            </p>
+            <IoIosArrowDroprightCircle className='arrow-btn' />
+          </div>
+
+          <section>
+            <div>
+              <Image
+                src='/image1-landingpage.jpg'
+                alt='Girl creating a form on the computer'
+                width={767}
+                height={767}
+              />
+            </div>
+            <p>
+              Empower your ideas and streamline your data collection process
+              with our intuitive online form creator.
+            </p>
+            <div>
+              <Image
+                src='/image2-landingpage.jpg'
+                alt='Guy writing a his idea on a paper on what to have on his custom form'
+                width={767}
+                height={767}
+              />
+            </div>
+            <p>
+              Whether you're designing a survey, registration form, or feedback
+              questionnaire, our platform allows you to effortlessly craft
+              custom forms tailored to your needs.
+            </p>
+            <div>
+              <Image
+                src='/image3-landingpage.jpg'
+                alt='Some wooden cirkles triangles and rectangels standing on eachother to illustrate building and creating your own form.'
+                width={767}
+                height={767}
+              />
+            </div>
+            <p>
+              Unleash the power of your imagination with our platform designed
+              to inspire creativity. Your creations, your way because innovation
+              knows no bounds.
+            </p>
+          </section>
+        </main>
       </div>
-
-      <section>
-        <div>
-          <Image
-            src='/image1-landingpage.jpg'
-            alt='Girl creating a form on the computer'
-            width={767}
-            height={767}
-          />
-        </div>
-        <p>
-          Empower your ideas and streamline your data collection process with
-          our intuitive online form creator.
-        </p>
-        <div>
-          <Image
-            src='/image2-landingpage.jpg'
-            alt='Guy writing a his idea on a paper on what to have on his custom form'
-            width={767}
-            height={767}
-          />
-        </div>
-        <p>
-          Whether you're designing a survey, registration form, or feedback
-          questionnaire, our platform allows you to effortlessly craft custom
-          forms tailored to your needs.
-        </p>
-        <div>
-          <Image
-            src='/image3-landingpage.jpg'
-            alt='Some wooden cirkles triangles and rectangels standing on eachother to illustrate building and creating your own form.'
-            width={767}
-            height={767}
-          />
-        </div>
-        <p>
-          Unleash the power of your imagination with our platform designed to
-          inspire creativity. Your creations, your way because innovation knows
-          no bounds.
-        </p>
-      </section>
-    </main>
+      <Footer />
+    </>
   )
 }
