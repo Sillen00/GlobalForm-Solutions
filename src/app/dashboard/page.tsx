b8966c41b32199733e834e0f1e63c695d4b517e6
Simon <sillen.00@hotmail.com>
2023-12-29 11:08:52 +0100

    npm run prettier.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,14 +10,12 @@ function DashboardPage() {
       <div className='dashboard-wrapper'>
         <h2>Dashboard</h2>
         <div className='form-card-container'>
-          
           {/* Create new form card: */}
           <Link href='/create-form'>
             <div className='create-new-card'>
               <FaPlus />
             </div>
           </Link>
-
           {/* Form cards: */} {/* Example cards....*/}
           <FormCard date='Datum' time='Tid' title='Titel' place='Plats' />
           <FormCard date='Datum' time='Tid' title='Titel' place='Plats' />
