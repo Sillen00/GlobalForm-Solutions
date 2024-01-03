22523d0b0985fb9284894fa131d8cce80434d6b8
Simon <sillen.00@hotmail.com>
2024-01-03 11:26:30 +0100

    removed metadata. Already exist in the root layout.

--- src/app/(create-form)/layout.tsx
+++ src/app/(create-form)/layout.tsx
@@ -1,10 +1,3 @@
-export const metadata = {
-  title: "GlobalForm Solutions",
-  description:
-    "Create forms and surveys with ease. Customise your forms to suit your needs.",
-  icons: [{ rel: "icon", url: "/favicon.png" }],
-}
-
 const CreateFormLayout = ({ children }: { children: React.ReactNode }) => {
   return <>{children}</>
 }
--- src/app/(pages-with-header-and-footer)/layout.tsx
+++ src/app/(pages-with-header-and-footer)/layout.tsx
@@ -1,12 +1,5 @@
 import Footer from "../_components/Footer"
 
-export const metadata = {
-  title: "GlobalForm Solutions",
-  description:
-    "Create forms and surveys with ease. Customise your forms to suit your needs.",
-  icons: [{ rel: "icon", url: "/favicon.png" }],
-}
-
 const HeaderFooterLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
