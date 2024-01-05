38592ea1116ae0422d73abe3d67c4fbe9c28bda7
Thomas ingvarsson <105804552+GunnarIng@users.noreply.github.com>
2024-01-05 10:47:45 +0100

    Added Classname prop and use client

--- src/app/_components/AccordionSection.tsx
+++ src/app/_components/AccordionSection.tsx
@@ -1,25 +1,26 @@
-"use client"
-import { useState } from "react"
-import "./AccordionSection.scss"
+"use client";
+import { useState } from "react";
+import "./AccordionSection.scss";
 
 interface AccordionSectionProps {
-  title: string
-  children: React.ReactNode
+  title: string;
+  children: React.ReactNode;
+  className?: string; // Add this line
 }
 
-const AccordionSection = ({ title, children }: AccordionSectionProps) => {
-  const [isOpen, setIsOpen] = useState(false)
+const AccordionSection = ({ title, children, className }: AccordionSectionProps) => {
+  const [isOpen, setIsOpen] = useState(false);
 
   const toggleAccordion = () => {
-    setIsOpen(!isOpen)
-  }
+    setIsOpen(!isOpen);
+  };
 
   return (
-    <div className={`accordion-section ${isOpen ? "active" : ""}`}>
+    <div className={`accordion-section ${isOpen ? "active" : ""} ${className}`}>
       <h2 onClick={toggleAccordion}>{title}</h2>
       <div className='content'>{children}</div>
     </div>
-  )
-}
+  );
+};
 
-export default AccordionSection
+export default AccordionSection;
