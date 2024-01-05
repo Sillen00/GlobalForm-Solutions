2b81315937a310694231f4d00291411efdbedfcd
stenbumling <marcusrosin@outlook.com>
2024-01-05 11:54:57 +0100

    handle some potential errors when creating user

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -14,8 +14,25 @@ export async function POST(req: Request) {
   try {
     const { type, data } = await verifyWebhook(req, WEBHOOK_SECRET)
 
+    // User handling
     switch (type) {
       case "user.created":
+        // Verify that the webhook data contains a valid user ID
+        if (!data.id) {
+          throw new Error("No valid user found in webhook data")
+        }
+
+        // Check if user already exists to ensure we don't create duplicates
+        const existingUser = await db.user.findUnique({
+          where: { clerkUserId: data.id },
+        })
+
+        if (existingUser) {
+          console.log(`User with ID ${data.id} already exists.`)
+          break
+        }
+
+        // Create user in database
         const newUser = await db.user.create({
           data: {
             clerkUserId: data.id,
@@ -29,8 +46,11 @@ export async function POST(req: Request) {
             clerkUserId: data.id,
           },
         })
-        console.log("User was successfully created in the database: ", deletedUser)
         // TODO: Also delete all associated forms and those forms associated blocks
+        console.log(
+          "User was successfully created in the database: ",
+          deletedUser
+        )
         break
       default:
         // TODO: Look up other useful cases in Clerk docs
