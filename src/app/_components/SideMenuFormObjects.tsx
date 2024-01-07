7c457d6b4487bf15327497fe2c7a488d53660b6b
Simon <sillen.00@hotmail.com>
2024-01-07 22:27:20 +0100

    added some begining styling to the side menu form objekt.

--- src/app/_components/SideMenuFormObjects.tsx
+++ src/app/_components/SideMenuFormObjects.tsx
@@ -1,13 +1,17 @@
+import { FaGripLines, FaTrashCan } from "react-icons/fa6"
+
 import "./SideMenuFormObjects.scss"
 
 function SideMenuFormObjects() {
   return (
     <div className='side-menu-form-objects'>
-      <div className="form-object">
-        <div>Trashcan</div>
-        <label id="form-title">Form Title</label>
-        <input id="form-title" type="text" />
-        <div>move icon.</div>
+      <div className='form-object'>
+        <FaTrashCan className='trashcan-icon' />
+        <div className='input-box'>
+          <label id='form-title'>Form Title</label>
+          <input id='form-title' type='text' />
+        </div>
+        <FaGripLines />
       </div>
     </div>
   )
