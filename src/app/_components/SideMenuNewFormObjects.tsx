b06779fa1206c70a9195dd6a43a2a3a5cec23633
Simon <sillen.00@hotmail.com>
2024-01-08 12:10:12 +0100

    setup style file and simple html structure.

--- src/app/_components/SideMenuNewFormObjects.tsx
+++ src/app/_components/SideMenuNewFormObjects.tsx
@@ -1,10 +1,19 @@
-import "./SideMenuNewFormObjects.scss"
+import styles from "./SideMenuNewFormObjects.module.scss"
 
 function SideMenuNewFormObjects() {
   return (
-    <div className='side-menu-new-objects'>
-      <h2>CreateFormPage add objects</h2>
+    <div className={styles.sideMenuNewObjects}>
       <p>Här ska man kunna lägga till nya objekt / inputs.</p>
+
+      <label id="object-title">Object title (required)</label>
+      <input id="object-title" type="text" name="object-title" required />
+
+      <p>Object types:</p>
+
+      <div>
+        <p>icon</p>
+        <p>Text object</p>
+      </div>
     </div>
   )
 }
