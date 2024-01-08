bda3c91924ac42bec3693b695e2baaf557ef4a2b
Simon <sillen.00@hotmail.com>
2024-01-08 14:48:24 +0100

    more html and css.

--- src/app/_components/SideMenuNewFormObjects.tsx
+++ src/app/_components/SideMenuNewFormObjects.tsx
@@ -3,14 +3,17 @@ import styles from "./SideMenuNewFormObjects.module.scss"
 function SideMenuNewFormObjects() {
   return (
     <div className={styles.sideMenuNewObjects}>
-      <label id='object-title'>Object title (required)</label>
-      <input id='object-title' type='text' name='object-title' required />
+      <div className={styles.inputBox}>
+        <label id='object-title'>Object title (required)</label>
+        <input id='object-title' type='text' name='object-title' required />
+      </div>
 
-      <p>Object types:</p>
-
-      <div>
-        <p>icon</p>
-        <p>Text object</p>
+      <div className={styles.blockTypeContainer}>
+        <p>Object types:</p>
+        <div>
+          <p>icon</p>
+          <p>Text object</p>
+        </div>
       </div>
     </div>
   )
