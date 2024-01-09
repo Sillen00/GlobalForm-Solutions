4bd64a9124290d8f75996ce89c9c6f2f951c1ffd
Simon <sillen.00@hotmail.com>
2024-01-09 10:31:13 +0100

    react state to handle form block click.

--- src/app/_components/SideMenuNewFormBlocks.tsx
+++ src/app/_components/SideMenuNewFormBlocks.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { CiLink } from "react-icons/ci"
 import { FaPhoneAlt, FaRegDotCircle } from "react-icons/fa"
 import { IoCalendarNumberSharp } from "react-icons/io5"
@@ -9,8 +10,15 @@ import { TiInputChecked } from "react-icons/ti"
 import styles from "./SideMenuNewFormBlocks.module.scss"
 
 function SideMenuNewFormBlocks() {
+  const [selectedBlock, setSelectedBlock] = useState<string | null>(null)
+
+  const handleFormBlockClick = (formBlock: string) => {
+    setSelectedBlock(formBlock)
+  }
+
   const addFormBlock = () => {
     console.log("add form block")
+    console.log(selectedBlock)
   }
 
   return (
@@ -22,53 +30,53 @@ function SideMenuNewFormBlocks() {
 
       <h4>Block types:</h4>
       <div className={styles.blockTypeContainer}>
-        <div onClick={addFormBlock}>
+        <div onClick={() => handleFormBlockClick("text")}>
           <TbLetterT />
           <p>Text</p>
         </div>
-        <div onClick={addFormBlock}>
+        <div onClick={() => handleFormBlockClick("textarea")}>
           <TbSquareLetterT />
           <p>Text Area</p>
         </div>
-        <div onClick={addFormBlock}>
+        <div onClick={() => handleFormBlockClick("textinput")}>
           <LuFormInput />
           <p>Text input</p>
         </div>
-        <div onClick={addFormBlock}>
+        <div onClick={() => handleFormBlockClick("numberinput")}>
           <TbNumbers />
           <p>Number input</p>
         </div>
-        <div onClick={addFormBlock}>
+        <div onClick={() => handleFormBlockClick("phoneinput")}>
           <FaPhoneAlt />
           <p>Phone input</p>
         </div>
-        <div onClick={addFormBlock}>
+        <div onClick={() => handleFormBlockClick("emailinput")}>
           <MdEmail />
           <p>Email input</p>
         </div>
-        <div onClick={addFormBlock}>
+        <div onClick={() => handleFormBlockClick("dateinput")}>
           <IoCalendarNumberSharp />
           <p>Date input</p>
         </div>
-        <div onClick={addFormBlock}>
+        <div onClick={() => handleFormBlockClick("urlinput")}>
           <CiLink />
           <p>Url input</p>
         </div>
-        <div onClick={addFormBlock}>
+        <div onClick={() => handleFormBlockClick("dropdowninput")}>
           <RxDropdownMenu />
           <p>Dropdown input</p>
         </div>
-        <div onClick={addFormBlock}>
+        <div onClick={() => handleFormBlockClick("radiobutton")}>
           <FaRegDotCircle />
           <p>Radio Button</p>
         </div>
-        <div onClick={addFormBlock}>
+        <div onClick={() => handleFormBlockClick("checkbox")}>
           <TiInputChecked />
           <p>Checkbox</p>
         </div>
       </div>
 
-      <button>Add block</button>
+      <button onClick={addFormBlock}>Add block</button>
     </div>
   )
 }
