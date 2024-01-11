92c70f43b00e476faaffaf166ca7d868a0601985
stenbumling <marcusrosin@outlook.com>
2024-01-11 13:05:13 +0100

    add validation schema for form

--- src/server/api/routers/form.ts
+++ src/server/api/routers/form.ts
@@ -2,7 +2,8 @@ import { z } from "zod"
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc"
 
-// Validation scehmas
+// Validation schemas
+
 const formBlockSchema = z.object({
   order: z.number(),
   title: z.string().optional(),
@@ -25,7 +26,20 @@ const formBlockSchema = z.object({
   options: z.array(z.string()).optional(),
 })
 
+const formSchema = z.object({
+  userId: z.string(),
+  title: z.string(),
+  startDate: z.string(),
+  endDate: z.string(),
+  startTime: z.string(),
+  endTime: z.string(),
+  location: z.string(),
+  description: z.string(),
+  blocks: z.array(formBlockSchema),
+})
+
 // Router
+
 export const formRouter = createTRPCRouter({
   getUserForms: publicProcedure.input(z.string()).query(({ ctx, input }) => {
     const forms = ctx.db.user.findUnique({
@@ -46,34 +60,20 @@ export const formRouter = createTRPCRouter({
     })
     return form
   }),
-  createForm: publicProcedure
-    .input(
-      z.object({
-        userId: z.string(),
-        title: z.string(),
-        startDate: z.string(),
-        endDate: z.string(),
-        startTime: z.string(),
-        endTime: z.string(),
-        location: z.string(),
-        description: z.string(),
-        blocks: z.array(z.string()),
-      })
-    )
-    .query(({ ctx, input }) => {
-      const form = ctx.db.form.create({
-        data: {
-          userId: input.userId,
-          title: input.title,
-          startDate: input.startDate,
-          endDate: input.endDate,
-          startTime: input.startTime,
-          endTime: input.endTime,
-          location: input.location,
-          description: input.description,
-          blocks: input.blocks,
-        },
-      })
-      return form
-    }),
+  createForm: publicProcedure.input(formSchema).query(({ ctx, input }) => {
+    const form = ctx.db.form.create({
+      data: {
+        userId: input.userId,
+        title: input.title,
+        startDate: input.startDate,
+        endDate: input.endDate,
+        startTime: input.startTime,
+        endTime: input.endTime,
+        location: input.location,
+        description: input.description,
+        blocks: input.blocks,
+      },
+    })
+    return form
+  }),
 })
