a350521563e62702b04d5ba80a604d21c7fe1fbc
Thomas ingvarsson <105804552+GunnarIng@users.noreply.github.com>
2024-01-16 15:14:01 +0100

    Add block from textinput to sidemenu. Can remove block also.

--- src/app/_components/SideMenuFormBlocks.tsx
+++ src/app/_components/SideMenuFormBlocks.tsx
@@ -1,51 +1,58 @@
 import { FaGripLines, FaTrashCan } from "react-icons/fa6"
 
+import { useState } from "react"
+import { useForm } from "../(contexts)/FormContext"
 import Button, { IconType } from "./Button"
 import styles from "./SideMenuFormBlocks.module.scss"
 
 function SideMenuFormBlocks() {
-  const removeFormBlock = () => {
-    // Function to remove the form block from the form
-    console.log("removeFormBlock")
-  }
+  const { formData, setFormData } = useForm()
+
+  const removeFormBlock = (index: number) => {
+    setFormData(prevFormData => {
+      const newFormBlocks = prevFormData.formBlocks.filter((_, i) => i !== index);
+      return { ...prevFormData, formBlocks: newFormBlocks };
+    });
+  };
 
   const moveBlock = () => {
     // Function to move the form block in the form
-    // Jag tänkder att vi kan börja med att bara ha funktionaliteten för att flytta ner ett form objekt.
     console.log("moveBlock")
   }
 
+  const handleDescriptionChange = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
+    const newFormBlocks = formData.formBlocks.map((block, i) => {
+      if (i === index) {
+        return { ...block, description: e.target.value };
+      }
+      return block;
+    });
+    setFormData({ ...formData, formBlocks: newFormBlocks });
+  };
+
   return (
     <div className={styles.sideMenuFormBlock}>
-      <div className={styles.formBlock}>
-        <FaTrashCan onClick={removeFormBlock} />
-        <label id='form-title'>Form Title</label>
-        <input id='form-title' type='text' />
-        <FaGripLines onClick={moveBlock} />
-      </div>
-      <div className={styles.formBlock}>
-        <FaTrashCan onClick={removeFormBlock} />
-        <label id='form-location'>Form Location</label>
-        <input id='form-location' type='text' />
-        <FaGripLines onClick={moveBlock} />
-      </div>
-      <div className={styles.formBlock}>
-        <FaTrashCan onClick={removeFormBlock} />
-        <label id='form-date'>Form Date</label>
-        <input id='form-date' type='text' />
-        <FaGripLines onClick={moveBlock} />
-      </div>
-      <div className={styles.formBlock}>
-        <FaTrashCan onClick={removeFormBlock} />
-        <label id='form-description'>Form Description</label>
-        <input id='form-description' type='text' />
-        <FaGripLines onClick={moveBlock} />
-      </div>
+      {formData.formBlocks.map((formBlock, index) => {
+        return (
+          <div className={styles.formBlock} key={index}>
+            <FaTrashCan onClick={() => removeFormBlock(index)} />
+            <label id={`form-title-${index}`}>{formBlock.title}</label>
+            <input
+              id={`form-description-${index}`}
+              type='text'
+              value={formBlock.description}
+              onChange={e => handleDescriptionChange(e, index)}
+            />
+            <FaGripLines onClick={moveBlock} />
+          </div>
+        )
+      })}
 
-      {/* todo: add rqurierments for button before enabled (zod?) */}
-      <div className={styles.iconContainer}>
-        <Button icon={IconType.Save}>Generate Form</Button>
-      </div>
+      {formData.formBlocks.length > 0 && (
+        <div className={styles.iconContainer}>
+          <Button icon={IconType.Save}>Generate Form</Button>
+        </div>
+      )}
     </div>
   )
 }
