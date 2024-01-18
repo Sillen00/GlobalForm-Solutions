79f788a0544e12ab45b2e6c186f3243b42549967
stenbumling <marcusrosin@outlook.com>
2024-01-18 18:19:21 +0100

    add input field to preview

--- src/app/_components/form/form-blocks/FormTextInput.tsx
+++ src/app/_components/form/form-blocks/FormTextInput.tsx
@@ -1,6 +1,8 @@
+import styles from "./FormTextInput.module.scss"
+
 export default function FormTextInput() {
   return (
-    <div>
+    <div className={styles.textInputContainer}>
       <input type='text'></input>
     </div>
   )
