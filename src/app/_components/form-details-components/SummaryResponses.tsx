aa68b88502b39f5b9ec526f365f5be86af6d0fcb
Simon <sillen.00@hotmail.com>
2024-01-22 20:57:36 +0100

    Funkar med error. Kolla med Marcus om DB typerna.

--- src/app/_components/form-details-components/SummaryResponses.tsx
+++ src/app/_components/form-details-components/SummaryResponses.tsx
@@ -4,7 +4,7 @@ import styles from "./SummaryResponses.module.scss"
 interface Response {
   id: string
   formId: string
-  answers: JsonValue
+  answers: JsonValue[] // CHECK WITH MARCUS TOMORROW! HERE IS PROBABLY THE PROBLEM! AND IN DB!
   createdAt: Date
   updatedAt: Date
 }
@@ -17,112 +17,103 @@ function SummaryResponses({
   if (!formDataResponses) {
     return <p>Loading...</p>
   }
+  console.log("formDataResponses", formDataResponses)
 
-  const fakeResponses = [
-    {
-      id: "string",
-      formId: "string",
-      answers: [
-        {
-          Question1: "Answerfromfirst user1",
-        },
-        {
-          Question2: "Answerfromfirst user2",
-        },
-        {
-          Question3: "Answerfromfirst user3",
-        },
-      ],
-      createdAt: "Date",
-      updatedAt: "Date",
-    },
-    {
-      id: "string",
-      formId: "string",
-      answers: [
-        {
-          Question1: "AnswerfromSECOND user1",
-        },
-        {
-          Question2: "AnswerfromSECOND user2",
-        },
-        {
-          Question3: "AnswerfromSECOND user3",
-        },
-      ],
-      createdAt: "Date",
-      updatedAt: "Date",
-    },
-  ]
-
-  const response1 = fakeResponses[0]
+  // const fakeResponses = [
+  //   {
+  //     id: "string",
+  //     formId: "string",
+  //     answers: [
+  //       {
+  //         Question1: "Answerfromfirst user1",
+  //       },
+  //       {
+  //         Question2: "Answerfromfirst user2",
+  //       },
+  //       {
+  //         Question3: "Answerfromfirst user3",
+  //       },
+  //     ],
+  //     createdAt: "Date",
+  //     updatedAt: "Date",
+  //   },
+  //   {
+  //     id: "string",
+  //     formId: "string",
+  //     answers: [
+  //       {
+  //         Question1: "AnswerfromSECOND user1",
+  //       },
+  //       {
+  //         Question2: "AnswerfromSECOND user2",
+  //       },
+  //       {
+  //         Question3: "AnswerfromSECOND user3",
+  //       },
+  //     ],
+  //     createdAt: "Date",
+  //     updatedAt: "Date",
+  //   },
+  //   {
+  //     id: "string",
+  //     formId: "string",
+  //     answers: [
+  //       {
+  //         Question1: "AnswerfromTHIRD user1",
+  //       },
+  //       {
+  //         Question2: "AnswerfromTHIRD user1",
+  //       },
+  //       {
+  //         Question3: "AnswerfromTHIRD user1",
+  //       },
+  //     ],
+  //     createdAt: "Date",
+  //     updatedAt: "Date",
+  //   },
+  // ]
 
   return (
     <div className={styles.form_responses_summary}>
-      <div>
-        <>
-          {(() => {
-            // Your .map() function logic goes here
-            const questionAndAnswerArray = response1?.answers
+      {(() => {
+        const firstQuestionAndAnswersArray = formDataResponses[0]?.answers
 
-            if (questionAndAnswerArray && questionAndAnswerArray.length > 0) {
-              return questionAndAnswerArray.map((answerObj, answerObjIndex) => (
-                <div key={answerObjIndex}>
-                  <strong>
-                    {Object.entries(answerObj).map(([question, value]) => (
-                      <>
-                        <div key={question}>{question}</div>
+        if (
+          firstQuestionAndAnswersArray &&
+          firstQuestionAndAnswersArray.length > 0
+        ) {
+          return firstQuestionAndAnswersArray.map(
+            (answerObj, answerObjIndex) => (
+              <strong key={answerObjIndex}>
+                {Object.entries(answerObj).map(([question], questionIndex) => (
+                  <>
+                    <h3 key={questionIndex}>{question}</h3>
 
-                        {/* Map out the answers only if the key/question is corelated to the answers. */}
-                        {fakeResponses.map((responseObj, responseIndex) => (
-                          <div key={responseIndex}>
-                            {responseObj.answers.map((answer, answerIndex) => (
-                              <div key={answerIndex}>
-                                <strong>
-                                  {Object.entries(answer).map(([key, value]) =>
-                                    question === key ? (
-                                      <div key={key}>{value}</div>
-                                    ) : null
-                                  )}
-                                </strong>
-                              </div>
-                            ))}
-                          </div>
+                    {formDataResponses?.map((responseObj, responseIndex) => (
+                      <div key={responseIndex}>
+                        {responseObj.answers.map((answer, answerIndex) => (
+                          <ul key={answerIndex}>
+                            {/* Map out the answers only if the key/question is corelated to the answers. */}
+                            {Object.entries(answer).map(
+                              ([questionCheck, answer], answersIndex) =>
+                                question === questionCheck ? (
+                                  <li key={answersIndex}>{answer}</li>
+                                ) : null
+                            )}
+                          </ul>
                         ))}
-                      </>
+                      </div>
                     ))}
-                  </strong>
-                </div>
-              ))
-            }
+                  </>
+                ))}
+              </strong>
+            )
+          )
+        }
 
-            // You can return null or any other component if there are no answers
-            return null
-          })()}
-        </>
-        {/* {fakeResponses.map((response, responseIndex) => (
-          <div key={responseIndex}>
-            {response.answers.map((answer, answerIndex) => (
-              <div key={answerIndex}>
-                <strong>
-                  {Object.entries(answer).map(([key, value]) => (
-                    <div key={key}>{value}</div>
-                  ))}
-                </strong>
-              </div>
-            ))}
-          </div>
-        ))} */}
-      </div>
-
-      <h4>FRÅGA</h4>
-      <p>4 Svar (visa antalet svar.)</p>
-      <ul>
-        <li>röd (svar 1)</li>
-        <li>grön (svar 2)</li>
-        <li>orange (svar 3)</li>
-        <li>röd (svar 4)</li>
-      </ul>
+        // You can return null or any other component if there are no answers
+        return null
+      })()}
     </div>
   )
 }
